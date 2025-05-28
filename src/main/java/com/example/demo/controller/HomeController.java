cdf5ca612b1096a2fbc5f99bce058c933eac3d83
kristinadd <kristina@productboard.com>
2025-05-28 20:23:41 +0200

    Upgrade home page

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
     
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("currentTime", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        model.addAttribute("currentDate", LocalDateTime.now().format(DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy")));
         return "home";
     }
-} 
+}
