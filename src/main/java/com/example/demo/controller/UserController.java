8bd683c3364ef38f6417a6be29c2726083a67c74
kristinadd <kristina@productboard.com>
2025-05-31 12:01:53 +0200

    fix cassandra

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.User;
 import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
