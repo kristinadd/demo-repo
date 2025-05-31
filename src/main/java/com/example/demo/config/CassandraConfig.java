d56a65eeab30d875c374819771fed28ad420ce60
kristinadd <kristina@productboard.com>
2025-05-31 12:20:02 +0200

    Fix cassandra connection

--- src/main/java/com/example/demo/config/CassandraConfig.java
+++ src/main/java/com/example/demo/config/CassandraConfig.java
@@ -1,31 +1,19 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
-import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    @Override
-    protected String getKeyspaceName() {
-        return "wick_prod";
-    }
+    @Value("${spring.cassandra.keyspace-name}")
+    private String keyspaceName;
 
     @Override
-    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification
-                .createKeyspace(getKeyspaceName())
-                .ifNotExists()
-                .with(KeyspaceOption.DURABLE_WRITES, true)
-                .withSimpleReplication(1);
-        
-        return Arrays.asList(specification);
+    protected String getKeyspaceName() {
+        return keyspaceName;
     }
 
     @Override
@@ -37,4 +25,4 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     public String[] getEntityBasePackages() {
         return new String[]{"com.example.demo"};
     }
-} 
+}
